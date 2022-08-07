c462be7dadaf405861435017b003f53b4f9727d7
junshan0 <junshan1@yahoo.com>
2022-08-07 11:03:24 -0400

    Add files via upload

--- Chapter09/Exercise9.02/Exercise9.02.sql
+++ Chapter09/Exercise9.02/Exercise9.02.sql
@@ -20,3 +20,83 @@ ON
 
 -- Step 4
 DROP TABLE IF EXISTS products_sales;
+
+-- Step 5
+SELECT 
+  *
+FROM 
+  products_sales 
+LIMIT 
+  5;
+
+-- Step 6
+SELECT 
+  * 
+FROM 
+  products_sales 
+WHERE 
+  model='Bat' 
+ORDER BY 
+  sales_date;
+
+-- Step 7
+SELECT 
+  COUNT(model) 
+FROM 
+  products_sales 
+WHERE 
+  model='Bat';
+
+-- Step 8
+SELECT 
+  MAX(sales_date) 
+FROM 
+  products_sales 
+WHERE 
+  model='Bat';
+
+-- Step 9
+SELECT 
+  * 
+INTO 
+  bat_sales 
+FROM 
+  products_sales 
+WHERE 
+  model='Bat' 
+ORDER BY 
+  sales_date;
+
+-- Step 10
+SELECT 
+  * 
+FROM 
+  bat_sales 
+ORDER BY 
+  sales_date
+LIMIT 
+  5;
+
+-- Step 11
+SELECT 
+  sales_date, 
+  COUNT(sales_date) 
+INTO 
+  bat_sales_daily 
+FROM 
+  bat_sales 
+GROUP BY 
+  sales_date 
+ORDER BY 
+  sales_date;
+
+-- Step 12
+SELECT 
+  * 
+FROM 
+  bat_sales_daily 
+ORDER BY 
+  sales_date
+LIMIT 
+  22;
+
